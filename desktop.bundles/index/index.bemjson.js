39488cb659bd014802610d2b0a18e2b9a532ba0e
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-04-17 01:10:22 +0300

    Add header box js

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -11,53 +11,59 @@
     content: [
         {
             block: 'head',
-            mix: [ { block: 'box' } ],
-            content: {
-                block: 'layout',
-                content: [
-                    {
-                        elem: 'left',
-                        content: {
-                            tag: 'form',
-                            attrs: { action: 'http://yandex.ru/yandsearch' },
-                            content: [
-                                {
-                                    block: 'input',
-                                    name: 'text',
-                                    val: 'Find'
-                                },
-                                {
-                                    block: 'button',
-                                    type: 'submit',
-                                    content: 'Search'
-                                }
-                            ]
+            mix: { block: 'box', js: true },
+            content: [
+                {
+                    block: 'layout',
+                    content: [
+                        {
+                            elem: 'left',
+                            content: {
+                                tag: 'form',
+                                attrs: { action: 'http://yandex.ru/yandsearch' },
+                                content: [
+                                    {
+                                        block: 'input',
+                                        name: 'text',
+                                        val: 'Find'
+                                    },
+                                    {
+                                        block: 'button',
+                                        type: 'submit',
+                                        content: 'Search'
+                                    }
+                                ]
+                            }
+                        },
+                        {
+                            elem: 'right',
+                            content: {
+                                block: 'logo',
+                                content: [
+                                    {
+                                        block: 'link',
+                                        url: 'http://ru.bem.info',
+                                        content: [
+                                            {
+                                                block: 'image',
+                                                attrs: { src: 'http://varya.me/online-shop-dummy/desktop.blocks/b-logo/b-logo.png' }
+                                            },
+                                            {
+                                                elem: 'slogan',
+                                                content: 'A new way of thinking'
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
                         }
-                    },
-                    {
-                        elem: 'right',
-                        content: {
-                            block: 'logo',
-                            content: [
-                                {
-                                    block: 'link',
-                                    url: 'http://ru.bem.info',
-                                    content: [
-                                        {
-                                            block: 'image',
-                                            attrs: { src: 'http://varya.me/online-shop-dummy/desktop.blocks/b-logo/b-logo.png' }
-                                        },
-                                        {
-                                            elem: 'slogan',
-                                            content: 'A new way of thinking'
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    }
-                ]
-            }
+                    ]
+                },
+                {
+                    block: 'box',
+                    elem: 'switcher'
+                }
+            ]
         },
         {
             block: 'goods',
