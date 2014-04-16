d9fed3c34e94cc9b9c14d3f88f529b696000b562
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-04-17 01:22:02 +0300

    Add contact page

---
+++ desktop.bundles/contact/contact.bemjson.js
@@ -0,0 +1,16 @@
+({
+    block: 'page',
+    title: 'contact',
+    head: [
+        { elem: 'css', url: '_contact.css' }
+    ],
+    scripts: [{ elem: 'js', url: '_contact.js' }],
+    content: [
+       {
+           block: 'content',
+           content: [
+               'block content'
+           ]
+       }
+    ]
+})
